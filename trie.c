3233499c7817f559bf60bbe53786efcede32972e
Thomas Huitema <thuitema35@gmail.com>
2024-07-15 00:10:52 -0400

    Added function documentation

--- trie.c
+++ trie.c
@@ -6,17 +6,29 @@
 static void trie_display_helper(TrieNode *curr, char *prefix);
 static TrieNode* trie_node_create(void);
 
+/*
+Returns a pointer to a new TrieNode with is_terminal set to false
+Time complexity: O(1)
+*/
 static TrieNode* trie_node_create(void) {
     return calloc(1, sizeof(TrieNode));
 }
 
+/*
+Returns a pointer to a Trie with its root initialized
+Time complexity: O(1)
+*/
 Trie* trie_init(void) {
     Trie *trie = malloc(sizeof(Trie));
     trie->root = trie_node_create();
     return trie;
 }
 
-/* Return true if word is inserted successfully */
+/*
+Inserts word into trie
+Returns true if word is inserted successfully, false otherwise
+Time complexity: O(N) where N is the length of the word being inserted
+*/
 int trie_insert(Trie *t, const char *word) {
     TrieNode *curr = t->root;
     for(int i = 0; i < strlen(word); i++) {
@@ -36,14 +48,21 @@ int trie_insert(Trie *t, const char *word) {
     return 1;
 }
 
-/* Prints the structure of the tree to stdout */
-/* Words are marked with an asterisk */
+/* 
+Prints the structure of the trie to stdout 
+Words (is_terminal is true) are marked with an asterisk 
+Time compexity: O(N) where N is the number of prefixes or nodes in the trie
+*/
 void trie_display(Trie *t) {
-    /* DFS for each index of the current node's array */
     TrieNode *curr = t->root;
     trie_display_helper(curr, "");
 }
 
+/*
+Helper function for trie_display()
+Prints the prefix at curr 
+Performs depth-first search on each child pointer
+*/
 static void trie_display_helper(TrieNode *curr, char *prefix) {
     /* Print word */
     for(int i = 0; i < strlen(prefix); i++) {
@@ -58,6 +77,7 @@ static void trie_display_helper(TrieNode *curr, char *prefix) {
     /* Loop through each character pointer */
     for(int i = 0; i < NUM_CHARS; i++) {
         if(curr->chars[i]) {
+            /* Append current char to prefix*/
             char c_to_str[2];
             c_to_str[1] = '\0';
             c_to_str[0] = CHAR_MIN + i;
